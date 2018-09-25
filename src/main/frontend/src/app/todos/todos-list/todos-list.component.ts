d1963cb776a80cbad7ee18e1194e2d5a1b832f11
amadzia <agnieszkamadzia@onet.pl>
2018-09-25 13:22:18 +0200

    The todo-list component layout modified. Closes #7

--- src/main/frontend/src/app/todos/todos-list/todos-list.component.ts
+++ src/main/frontend/src/app/todos/todos-list/todos-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Todo} from "../todo.model";
 
 @Component({
   selector: 'app-todos-list',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosListComponent implements OnInit {
 
+  todos: Todo[]=[];
+
   constructor() { }
 
   ngOnInit() {
+      this.todos.push(new Todo(1, "Todo #1", true, "09/25/18"));
+      this.todos.push(new Todo(2, "Todo #2", false, "09/25/18"));
+      this.todos.push(new Todo(3, "Todo #3", false, "09/25/18"));
   }
 
+    onTodoChange(event, todo: Todo) {
+        console.log("Todo has changed");
+    }
+
+    getDueDateLabel(todo: Todo) {
+        return todo.completed ? 'badge badge-success' : 'badge badge-primary';
+    }
 }
