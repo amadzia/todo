48f3b0c2e73f5773985924d38390f13105751497
amadzia <agnieszkamadzia@onet.pl>
2018-09-25 11:46:47 +0200

    Components added. Closes #4

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -2,10 +2,16 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import { TodosAddComponent } from './todos/todos-add/todos-add.component';
+import { TodosListComponent } from './todos/todos-list/todos-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodosAddComponent,
+    TodosListComponent
   ],
   imports: [
     BrowserModule
